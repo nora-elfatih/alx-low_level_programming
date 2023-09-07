589ff421504d3a601a54ed2053fb36f9ed656dbb
nora-elfatih <noraalfatih97@gmail.com>
2023-09-07 12:15:05 -0700

    Write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
- * main - Entry
+ * main - Entry point
  *
  * Description: A programme without using printf anf puts
  *
@@ -11,6 +11,6 @@
 
 int main(void)
 {
-		write(1, and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",  59);
+		write(2, and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	rerturn (1);
 }
