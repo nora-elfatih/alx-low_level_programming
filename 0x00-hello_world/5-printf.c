bbd8f4182e89377ff4c4fc0791c58a466a5d1f1d
nora-elfatih <noraalfatih97@gmail.com>
2023-09-07 10:32:19 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entery  point
+ *
+ * Description: A C programme that prints using printf
+ *
+ * Return: This return 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
