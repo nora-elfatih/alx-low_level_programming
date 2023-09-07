726e84864f546d7f0268890f7e6615057205b393
nora-elfatih <noraalfatih97@gmail.com>
2023-09-07 12:21:05 -0700

    Write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,9 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
  * main - Entry point
  *
- * Description: A programme without using printf anf puts
+ * Description: A programme using write
  *
  * Return :This return 1
  */
@@ -12,5 +11,5 @@
 int main(void)
 {
 		write(2, and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	rerturn (1);
+	rerturn(1);
 }
