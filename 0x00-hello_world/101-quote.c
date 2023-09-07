1c42efd143f7ea980e0fb8330ffe8fd0d12b720c
nora-elfatih <noraalfatih97@gmail.com>
2023-09-07 13:30:36 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,15 +3,15 @@
 /**
  * main - Entry point
  *
- * Description: A programme using write to print a quote
+ * Description: A C programme using write to print a quote
  *
  * Return :This return 1
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	char qte[]  = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
+	write(1, qte, 59);
 	return (1);
-	
 }
