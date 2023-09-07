a5897d1b1f5b994161c030f9c45ebf0db060fcae
nora-elfatih <noraalfatih97@gmail.com>
2023-09-07 11:50:14 -0700

    Return 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Description: A programme without using printf anf puts
+ *
+ * Return :This return 1
+ */
+
+int main(void)
+{
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	rerturn (1);
+}
