ddd57b1fec7367aa4d24be79c865927ab9b0e1d1
nora-elfatih <noraalfatih97@gmail.com>
2023-09-08 12:36:53 -0700

    Aa to Zz

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C programme that print alphabet in lower and upper case
+ *
+ * Return: This return 0
+ */
+
+int main(void)
+{
+	char  a;
+	char  A;
+
+	a = 'a';
+	A = 'A';
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	while (A <= 'Z')
+	{
+		putchar(A);
+		A++;
+	}
+	putchar('\n');
+	return (0);
+}
