885b43e9f87766a518ea800fb2fd4f1959dd2c01
nora-elfatih <noraalfatih97@gmail.com>
2023-09-08 14:25:55 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/**
+ * main - Entry poinnt
+ *
+ * Description: A Programme that prints Hexadecimal numbers
+ *
+ * Return: This return 0
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	char a = 'a';
+
+	while (n >= 9)
+	{
+		putchar(n);
+		n++;
+	}
+	while (a >= 'f')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
+
