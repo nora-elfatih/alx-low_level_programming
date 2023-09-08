de83d942ddf6230b62390e9530279ae03530c4f8
nora-elfatih <noraalfatih97@gmail.com>
2023-09-08 13:30:00 -0700

    0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A programme that print number of single digit
+ *
+ * Return: This return 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
+
