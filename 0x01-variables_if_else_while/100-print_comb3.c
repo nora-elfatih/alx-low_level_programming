3b364865cf4e087b2e8fbecdd76951c6090115f6
nora-elfatih <noraalfatih97@gmail.com>
2023-09-09 05:03:14 -0700

    comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,11 +11,11 @@
 int main(void)
 {
 	int a = 0;
-	int b = 1;
+	int b = a + 1;
 
 	for (a = 0; a < 10; a++)
 	{
-		for (b = 1; b < 10; b++)
+		for (b = a + 1; b < 10; b++)
 		{
 			putchar('0' + a);
 			putchar('0' + b);
