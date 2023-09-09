1fb1b86255a621922f8f20d28a064825121e2258
nora-elfatih <noraalfatih97@gmail.com>
2023-09-09 05:57:43 -0700

    two two comb

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,18 +11,18 @@
 int main(void)
 {
 	int a = 0;
-	int b = a;
+	int b = a + 1;
 
 	for (a = 0; a <= 99; a++)
 	{
-		for (b = a; b <= 99; b++)
+		for (b = a + 1; b <= 99; b++)
 		{
 			putchar('0' + (a / 10));
 			putchar('0' + (a % 10));
 			putchar(' ');
 			putchar('0' + (b / 10));
 			putchar('0' + (b % 10));
-			if (a != 99 || b != 99)
+			if (a != 98 || b != 99)
 			{
 				putchar(',');
 				putchar(' ');
