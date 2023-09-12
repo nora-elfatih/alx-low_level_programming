8b2bac20daa5fa7d5a68126ba68e7e50a58a4f41
nora-elfatih <noraalfatih97@gmail.com>
2023-09-12 15:03:34 -0700

    time table 15

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * print_times_table  -  Prints times tables of n
+ *
+ * starting with 0
+ *
+ * @n: vailue to be print
+ */
+
+void print_times_table(int n)
+{
+	int l;
+	int m;
+	int o;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (l = 0; l <= n; l++)
+		{
+			_putchar('0');
+
+			for (m = 0; m <= n; m++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				o = l * m;
+
+				if (o <= 9)
+					_putchar(' ');
+				if (o <= 99)
+					_putchar(' ');
+				if (o <= 100)
+				{
+					_putchar((o / 100) + '0');
+					_putchar((o / 100) % 10 + '0');
+				}
+				else if (o <= 99 && o >= 10)
+				{
+					_putchar((o / 10) + '0');
+					_putchar((o % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
