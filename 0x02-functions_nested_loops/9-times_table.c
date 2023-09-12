85e4d35b1e36f12fc900b0ef101a7eda4f16ded6
nora-elfatih <noraalfatih97@gmail.com>
2023-09-12 12:15:04 -0700

    9 tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,22 +15,24 @@ void times_table(void)
 	for (a = 0; a <= 9; a++)
 	{
 		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
 
 		for (b = 0; b <= 9; b++)
 		{
+			_putchar(',');
+			_putchar(' ');
+
 			c = (a * b);
 
-			if ((c / 10) > 0)
-			{
-				_putchar((c / 10) + '0');
-			}
-			else
+			if (c <= 9)
 			{
 				_putchar(' ');
 			}
+			else
+			{
+				_putchar((c / 10) + '0');
+			}
 			_putchar ((c % 10) + '0');
 		}
+		_putchar('\n');
 	}
 }
