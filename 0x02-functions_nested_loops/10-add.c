72dd1c897992ecc78203a253c9a598c14f5d5991
nora-elfatih <noraalfatih97@gmail.com>
2023-09-12 12:35:38 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Add two numbers
+ *
+ * @a: Firt input
+ *
+ * @b: Second input
+ *
+ * Return: Return sum of a & b
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
