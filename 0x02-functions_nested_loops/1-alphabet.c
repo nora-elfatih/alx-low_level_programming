7033219edf95a5b964927083039ecd9b990fca39
nora-elfatih <noraalfatih97@gmail.com>
2023-09-12 06:38:40 -0700

    a to z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		_putchar(l);
+	}
+	_putchar('\n');
+	return (0);
+}
+
