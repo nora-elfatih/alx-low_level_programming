b591473c6f1fd95d0c183e7888208d46f795ccf9
nora-elfatih <noraalfatih97@gmail.com>
2023-09-13 05:09:12 -0700

    mult 3 & 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Programme prints number less than 1024 mult by 3 or 5
+ *
+ * Return: This return 0
+ */
+
+int main(void)
+{
+	int l = 1024;
+	int s = 0;
+	int i;
+
+	for (i = i; i < l; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			s += i;
+		}
+	}
+	printf("%d\n", s);
+	return (0);
+}
