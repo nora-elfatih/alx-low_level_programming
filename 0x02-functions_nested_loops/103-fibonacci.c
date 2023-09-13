9a1faff9a4f058ef05b626619c8d20c0078103e8
nora-elfatih <noraalfatih97@gmail.com>
2023-09-13 08:05:21 -0700

    103-fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include<main.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Programe add previous to term to  Fibonacci sequence
+ *
+ * Return: This return 0
+ */
+
+int main(void)
+{
+	int a = 0;
+	long b = 1;
+	long c = 2;
+
+	sum = c;
+
+	while (c + b <= 4000000)
+	{
+		c += b;
+		if (c % 2 == 0)
+		sum += c;
+		b = c - b;
+		a++;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
+
