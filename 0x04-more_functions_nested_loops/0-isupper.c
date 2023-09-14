1cfac5b3fae5926714ae3d7a009c91cdc0d21119
nora-elfatih <noraalfatih97@gmail.com>
2023-09-14 06:14:47 -0700

    upper c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include<stdio.h>
 
 /**
  * _isupper - Checks for uppercase character
@@ -12,11 +13,7 @@ int _isupper(int c)
 {
 
 	if (c >= 'A' && c <= 'Z')
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
