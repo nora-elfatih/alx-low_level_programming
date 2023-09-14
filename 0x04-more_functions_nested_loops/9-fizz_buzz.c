68f6d07473000533f86eed6a1069c891e3296e33
nora-elfatih <noraalfatih97@gmail.com>
2023-09-14 10:33:47 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Print  1 to 100 fizz buzz fizzbuzz
+ *
+ * Return: This return 0
+ */
+
+int main(void)
+{
+	int i = 1;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
