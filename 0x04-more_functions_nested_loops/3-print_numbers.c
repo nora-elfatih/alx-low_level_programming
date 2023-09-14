550ce45f4defe3fdd6ee26eac6d3dc11f9f04ee2
nora-elfatih <noraalfatih97@gmail.com>
2023-09-14 07:55:41 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print numbers from 0 to 9
+ *
+ * Return: This Return void
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
