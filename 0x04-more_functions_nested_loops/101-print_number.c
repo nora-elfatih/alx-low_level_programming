e5ffb87c665cb6de52445d28438795df9597bd53
nora-elfatih <noraalfatih97@gmail.com>
2023-09-14 12:48:01 -0700

    print_numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,9 +3,9 @@
 /**
  * print_number - Prints integers
  *
- * @n: Integer to be printed
+ * @n: Input integer
  *
- * Return: This return void
+ * Return: This return 0
  */
 
 void print_number(int n)
@@ -15,7 +15,7 @@ void print_number(int n)
 		_putchar('-');
 		n = -n;
 	}
-	if (n / 10 != 0)
+	if (n / 10)
 	{
 		print_number(n / 10);
 	}
