fdd3767d961f9b34bff20c9f8746e74f5dad6c86
nora-elfatih <noraalfatih97@gmail.com>
2023-09-19 13:14:42 -0700

    6puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,12 +10,12 @@
 
 void puts2(char *str)
 {
-	int a = 0;
+	int a;
 
-	while (str[a] != '\0')
+	for (a = 0; str[a] != '\0'; ++a)
 	{
-		_putchar(str[a]);
-		a += 2;
+		if (a % 2 == 0)
+			_putchar(str[a]);
 	}
 	_putchar('\n');
 }
