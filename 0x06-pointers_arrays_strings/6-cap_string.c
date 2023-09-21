b3ac160ccb5769097e827555befc4342c584f4d2
nora-elfatih <noraalfatih97@gmail.com>
2023-09-21 08:37:45 -0700

    cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include"main.h"
+
+/**
+ * cap_string - Capitalizes all words of a string
+ *
+ * @s: String to be capitalized
+ *
+ * Return: Return capitalized string
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		while (!(s[i] >= 'a' && s[i] <= 'z'))
+		{
+		       i++;
+		}
+		if (s[i - 1] == ' ' || s[i - 1] == '\t' || s[i - 1] == '\n' || s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' || s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' || s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' || s[i - 1] == '}' || i == 0)
+		{
+			s[i] -= 32;
+		}
+		i++;
+	}
+	return (s);
+}
+
+
+
