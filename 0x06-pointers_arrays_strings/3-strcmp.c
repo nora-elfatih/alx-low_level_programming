963df63a1760fab1ad069ec29f8da06f620d0d2d
nora-elfatih <noraalfatih97@gmail.com>
2023-09-21 04:44:56 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include"main.h"
+
+/**
+ * _strcmp - Compares two strings
+ *
+ * @s1: String 1
+ *
+ * @s2: String 2
+ *
+ * Return: Return  s1[i] - s2[i]
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
