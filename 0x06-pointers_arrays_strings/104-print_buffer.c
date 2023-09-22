b1263c7e9e8c5bbfbaca55dfc79c32b829875bfe
nora-elfatih <noraalfatih97@gmail.com>
2023-09-21 17:04:20 -0700

    buffer

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,17 @@
+#include"main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - Prints a buffer
+ *
+ * @b: buffer to be printed
+ *
+ * @size: Size of buffer
+ *
+ * Return: Return void
+ */
+
+void print_buffer(char *b, int size)
+{
+	print_buffer(b, size);
+}
