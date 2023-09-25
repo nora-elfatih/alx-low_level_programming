d8bb5ca6350b2183b241edc893ec1bbfe54710e2
nora-elfatih <noraalfatih97@gmail.com>
2023-09-25 15:47:31 -0700

    my first commit

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+* print_diagsums - prints the sum two square matrix of integers
+* @a: array of int type
+* @size: size of array
+* Return: nothing
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int i, s1 = 0, s2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		s1 += a[i];
+		s2 += a[size - i - 1];
+		a += size;
+	}
+	printf("%d, ", s1);
+	printf("%d\n", s2);
+}
